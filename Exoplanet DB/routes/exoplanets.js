9dbb45d9451a8d160a333f104189ec7e9af7c984
ZRCrawford15 <48399752+ZRCrawford15@users.noreply.github.com>
2021-07-29 21:45:27 -0700

    Add files via upload

--- Exoplanet DB/routes/exoplanets.js
+++ Exoplanet DB/routes/exoplanets.js
@@ -5,7 +5,7 @@ const exoplanets = require('../exoplanets');
 /* GET programming languages. */
 router.get('/', function(req, res, next) {
   try {
-    res.json(await exoplanets.getMultiple(req.query.page));
+    res.json(exoplanets.getMultiple(req.query.page));
   } catch (err) {
     console.error(`Error while getting exoplanets `, err.message);
     next(err);
